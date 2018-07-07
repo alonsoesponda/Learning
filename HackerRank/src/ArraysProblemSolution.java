e582ce97c723ed41f0718bc31d79e245e8cb0c7c
Alonso Esponda <alonso.esponda@gmail.com>
2018-07-06 19:25:44 -0500

    Adding reverseArray solution

---
+++ HackerRank/src/ArraysProblemSolution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArraysProblemSolution {
+
+    static void reverseArray(int[] arr){
+        for(int x=arr.length-1;x>=0;x--){
+            System.out.print(arr[x]);
+            if(x>0){
+                System.out.print(" ");
+            }
+        }
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        reverseArray(arr);
+
+        scanner.close();
+    }
+}
